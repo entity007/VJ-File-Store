3eaf68093d5229799c39ee64ea7bfc714643a954
entity007 <nileshraj3705@gmail.com>
2025-03-04 21:06:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,81 +2,88 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+
 import re
 import os
 from os import environ
-from Script import script  # Ensure this is a valid import
+from Script import script
 
-# Regex for ID Validation
 id_pattern = re.compile(r'^.\d+$')
-
-# Function to Check Boolean Environment Variables
-def is_enabled(value, default=False):
-    return str(value).lower() in ["true", "yes", "1", "enable", "y"]
-
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+      
 # Bot Information
 API_ID = int(environ.get("API_ID", "25195586"))
 API_HASH = environ.get("API_HASH", "32d15d1f3d4f8c1fe7bb21bed28ffb37")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-# Bot Start Picture
-PICS = environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg').split()
-
-# Admins List Parsing
+PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1161930116').split()]
+BOT_USERNAME = environ.get("BOT_USERNAME", "Toffeedistributerbot") # without @
+PORT = environ.get("PORT", "8080")
 
-# Bot Username
-BOT_USERNAME = environ.get("BOT_USERNAME", "Toffeedistributerbot")  # Without @
+# Clone Info :-
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
-# Server Port (Ensure Integer)
-PORT = int(environ.get("PORT", 8080))
+# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
+CDB_NAME = environ.get("CDB_NAME", "")
 
-# Clone Mode
-CLONE_MODE = is_enabled(environ.get('CLONE_MODE', "False"))
-
-# If Clone Mode is Enabled, Ensure Required Variables are Set
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "") if CLONE_MODE else ""
-CDB_NAME = environ.get("CDB_NAME", "") if CLONE_MODE else ""
-
-# Database Information (Ensure Proper Credentials)
-DB_URI = environ.get("DB_URI", "mongodb+srv://<db_username>:<password>@cluster0.fbqjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = environ.get("DB_NAME", "toffee_distributer")
+# Database Information
+DB_URI = environ.get("DB_URI", "mongodb+srv://<db_username>:<db_pHMhsbH8pwN#j-@>@cluster0.fbqjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "toffee distributer")
 
 # Auto Delete Information
-AUTO_DELETE_MODE = is_enabled(environ.get('AUTO_DELETE_MODE', "False"))
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) if AUTO_DELETE_MODE else 0  # Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) if AUTO_DELETE_MODE else 0  # Seconds
+AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', False)) # Set True or False
 
-# Logging Channel (Ensure Integer)
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "1002309172456"))
+# If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
-# File Caption Handling (Ensure script.CAPTION Exists)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", getattr(script, "CAPTION", "Default Caption"))
+# Channel Information
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "1002347269126"))
+
+# File Caption Information
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
-# Public File Store Mode
-PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"))
+# Enable - True or Disable - False
+PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
-# Verification Mode
-VERIFY_MODE = is_enabled(environ.get('VERIFY_MODE', "True"))
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "https://modijiurl.com/") if VERIFY_MODE else ""
-SHORTLINK_API = environ.get("SHORTLINK_API", "e905235ae9ef0a396c01e5818e7d6ccdd84d7b04") if VERIFY_MODE else ""
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/toffeedistributer") if VERIFY_MODE else ""
+# Verify Info :-
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
-# Website Information
-WEBSITE_URL_MODE = is_enabled(environ.get('WEBSITE_URL_MODE', "True"))
-WEBSITE_URL = environ.get("WEBSITE_URL", "https://www.blogger.com/u/2/blog/post/edit/2678387556182342654/5084359522687669244") if WEBSITE_URL_MODE else ""
+# If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "https://modijiurl.com/") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "e905235ae9ef0a396c01e5818e7d6ccdd84d7b04") # shortlink api
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/toffeedistributer") # how to open link 
 
-# File Stream Configuration
-STREAM_MODE = is_enabled(environ.get('STREAM_MODE', "False"))
+# Website Info:
+WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
+
+# If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://www.blogger.com/u/2/blog/post/edit/2678387556182342654/5084359522687669244") # For More Information Check Video On Yt - @Tech_VJ
+
+# File Stream Config
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
+
+# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-
-# Detect Heroku Environment
-ON_HEROKU = 'DYNO' in environ
+if 'DYNO' in environ:
+    ON_HEROKU = True
+else:
+    ON_HEROKU = False
 URL = environ.get("URL", "")
+
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+    
